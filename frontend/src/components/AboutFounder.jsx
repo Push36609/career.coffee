a1cf31e7cd3cddcf734778672142463d356bc2a2
pushpendra <ppal36609@gmail.com>
2026-04-26 11:30:13 +0530

    Fix casing for AboutFounder.jsx

--- frontend/src/components/AboutFounder.jsx
+++ frontend/src/components/AboutFounder.jsx
@@ -5,7 +5,7 @@ import founderImg from "../../public/gallery/founder.png";
 
 const AboutFounder = () => {
   return (
-    <section className="py-10 border-b bg-white overflow-hidden">
+    <section className="py-24 bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col lg:flex-row gap-1 items-start">
 
